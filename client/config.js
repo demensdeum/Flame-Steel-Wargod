d370cfc44001747ed933f07252ba3cad5662c4d6
Ilia Prokhorov <demensdeum@gmail.com>
2025-03-01 23:20:30 +0400

    new mobile version

--- client/config.js
+++ client/config.js
@@ -1,8 +1,8 @@
 // Game version and build configuration
 export const config = {
-    version: '0.1.0',
+    version: '0.2.0',
     buildId: new Date().toISOString().replace(/[^0-9]/g, '').slice(0, 14), // YYYYMMDDHHmmss
-    buildName: 'Alpha Wolf', // Cool codename for this version
+    buildName: 'Mobile Hunter', // Now with mobile controls!
     fullVersion: function() {
         return `${this.version}-${this.buildName}-${this.buildId}`;
     }
